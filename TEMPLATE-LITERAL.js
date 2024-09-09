a24282831784b5d52466dd6cd2362ee97818214b
Ayish-Shaikh <ayishshaikh.8805@gmail.com>
2024-09-09 20:25:14 +0530

    Template-literal-example

---
+++ TEMPLATE-LITERAL.js
@@ -0,0 +1,7 @@
+let obj = {
+    item : "pen",
+    price : "10",
+    quantity : "30pcs"
+};
+let output =  `the cost of ${obj.item} is ${obj.price} rupees and the quantity is ${obj.quantity}`;
+console.log(output);
