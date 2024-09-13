3ac2b9d4837c7036dd0bd86256e2ab18eaa82d66
Ayish-Shaikh <ayishshaikh.8805@gmail.com>
2024-09-13 23:52:02 +0530

    ADDS ONE OR MORE ELEMENT AT THE BEGINNING OF AND ARRAY.

---
+++ unshift().js
@@ -0,0 +1,5 @@
+let marvelHeroes = ["thor", "spiderman", "hulk", "ironman", "captainamerica"];
+
+//unshift() method adds one or more elements to the beginning of an array and returns the new length of the array.
+let newHeroes = marvelHeroes.unshift ("blackpanther");
+console.log(marvelHeroes);
