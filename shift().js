a771cf6a5d1c5f84ac72c9ea8a7eca3c2f3fafaf
Ayish-Shaikh <ayishshaikh.8805@gmail.com>
2024-09-13 23:55:01 +0530

    REMOVES THE FIRST ELEMENT FROM AN ARRAY

---
+++ shift().js
@@ -0,0 +1,5 @@
+let marvelHeroes = ["thor", "spiderman", "hulk", "ironman", "captainamerica"];
+
+//shift() method removes the first element from an array and returns that element.
+let newHeroes = marvelHeroes.shift ("blackpanther");
+console.log(marvelHeroes);
