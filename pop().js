9da11df5501fd2e7829fa8807170aad5a75c4a49
Ayish-Shaikh <ayishshaikh.8805@gmail.com>
2024-09-13 22:40:46 +0530

    REMOVES THE LAST ELEMENT

---
+++ pop().js
@@ -0,0 +1,7 @@
+let veggies = ["brinjal", "carrot", "tomato", "onion"];
+
+console.log(veggies);
+let deletedveggie=veggies.pop();
+//pop() method removes the last element of an array and returns that element .
+console.log(veggies);
+console.log("deleted", deletedveggie);
