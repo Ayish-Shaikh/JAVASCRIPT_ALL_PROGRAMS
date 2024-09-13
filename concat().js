ba919a345de56f28ef418df9409ccc6a679ad445
Ayish-Shaikh <ayishshaikh.8805@gmail.com>
2024-09-13 23:43:23 +0530

    MERGING OF EXISTING ARRAYS

---
+++ concat().js
@@ -0,0 +1,6 @@
+let marvelHeroes = ["thor", "spiderman", "hulk", "ironman", "captainamerica"];
+let dcHeroes = ["wonderwoman", "superman", "batman", "aquaman", "flash"];
+
+//concat() method creates a new array by merging (concatenating) existing arrays.
+let allHeroes = marvelHeroes.concat(dcHeroes);
+console.log(allHeroes);
