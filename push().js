6e6ea45a146d23fd32c53882032be17164bd0461
Ayish-Shaikh <ayishshaikh.8805@gmail.com>
2024-09-13 22:17:23 +0530

    ADDS AN ELEMENT

---
+++ push().js
@@ -0,0 +1,5 @@
+let veggies = ["brinjal", "carrot", "tomato", "onion"];
+
+veggies.push("biiterourd", "potato", "lentil");
+//push() method adds an element to the end of an array and returns the new length of the array .
+console.log(veggies);
