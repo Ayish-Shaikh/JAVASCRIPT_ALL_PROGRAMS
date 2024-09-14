7b377094b6723c3f74556699dcf217ca7dbfb5dd
Ayish-Shaikh <ayishshaikh.8805@gmail.com>
2024-09-14 23:59:09 +0530

    USING BASIC FUNCTION AND ARROW FUNCTION

---
+++ function.js
@@ -0,0 +1,24 @@
+//USING BASIC FUNCTIONS
+function countVowels(str){
+    let count=0;
+    for (const char of str){
+        if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u'){
+            count++;
+
+        }
+    }
+    return count;
+}
+
+//USING ARROW FUNCTON
+
+let countVowels2 = (str) => {
+    let count=0;
+    for (const char of str){
+        if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u'){
+            count++;
+
+        }
+    }
+    return count;
+}
