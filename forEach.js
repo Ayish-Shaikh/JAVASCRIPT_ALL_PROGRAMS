44d64f70e17d8257cecb93690f192398452642f1
Ayish-Shaikh <ayishshaikh.8805@gmail.com>
2024-09-15 09:43:25 +0530

    USED TO ITERATE OVER AN ARRAY AND PERFORM SOME TASKS

---
+++ forEach.js
@@ -0,0 +1,7 @@
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//forEach method is used to iterate over an array and perform some action
+// on each element of the array using a callback function passed to it.
+nums.forEach((num) => {
+    console.log(num * num);
+});
